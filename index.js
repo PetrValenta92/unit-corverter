47f69e914129eb791a3406368f06288475615b76
Petr Valenta <petr.valenta.92@gmail.com>
2023-02-14 23:00:38 +0100

    Conditions and alerts added

--- index.js
+++ index.js
@@ -11,15 +11,25 @@ const volumeOutputEl = document.getElementById("volume-output-el");
 const massOutputEl = document.getElementById("mass-output-el");
 
 
-function render(inputNum, toFeet, toMeters, toGallons, toLiters, toPounds, toKilos) {
-    lengthOutputEl.textContent = `${inputNum} meters = ${toFeet.toFixed(3)} feet | ${inputNum} feet = ${toMeters.toFixed(3)} meters`;
-    volumeOutputEl.textContent = `${inputNum} liters = ${toGallons.toFixed(3)} gallons | ${inputNum} gallons = ${toLiters.toFixed(3)} liters`;
-    massOutputEl.textContent = `${inputNum} kilos = ${toPounds.toFixed(3)} pounds | ${inputNum} pounds = ${toKilos.toFixed(3)} kilos`;
+function render(inputNum, foot, meter, gallon, liter, pound, kilo) {
+    
+    lengthOutputEl.textContent = `${inputNum} meters = ${foot.toFixed(3)} feet | ${inputNum} feet = ${meter.toFixed(3)} meters`;
+    
+    volumeOutputEl.textContent = `${inputNum} liters = ${gallon.toFixed(3)} gallons | ${inputNum} gallons = ${liter.toFixed(3)} liters`;
+    
+    massOutputEl.textContent = `${inputNum} kilos = ${pound.toFixed(3)} pounds | ${inputNum} pounds = ${kilo.toFixed(3)} kilos`;
 }
 
 convertBtn.addEventListener("click", function() {
-    if (inputEl.value) {
-        let inputNum = Number(inputEl.value);
+    let inputNum = Number(inputEl.value);
+    
+    if (!inputEl.value) {
+        alert("Please, add numbers to convert!");
+          
+    } else if (isNaN(inputNum)) {
+        alert("Sorry, only numbers are accepted. (For decimals use a point '.')");
+        
+    } else {
         
         let metToFeet = inputNum * 3.2808399;
         let feetToMet = inputNum / 3.2808399;
@@ -30,8 +40,6 @@ convertBtn.addEventListener("click", function() {
         let kiloToPound = inputNum * 2.20462262;
         let poundToKilo = inputNum / 2.20462262;
         
-        render(inputEl.value, metToFeet, feetToMet, litToGall, gallToLit, kiloToPound, poundToKilo);
-    } else {
-        alert("Please add some numbers to convert 😊");     
-    }      
+        render(inputEl.value, metToFeet, feetToMet, litToGall, gallToLit, kiloToPound, poundToKilo) 
+    }
 })
